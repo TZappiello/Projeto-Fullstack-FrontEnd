7a6aeff791433808390f378f7068889be652a68f
Tzappiello <tzappiello@gmail.com>
2022-06-28 18:31:55 -0300

    Criando Component Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,11 +29,13 @@ import { MatCardModule } from '@angular/material/card';
 
 // Aqui vai ficar os imports dos components criados para o projeto
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
