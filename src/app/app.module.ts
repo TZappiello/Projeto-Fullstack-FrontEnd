52d2d7c143ba4079dadaa4aff1d2d27afa890de9
lailaz <88406056+zallih@users.noreply.github.com>
2022-06-28 14:05:51 -0300

    Criação do component Nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,10 +26,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
