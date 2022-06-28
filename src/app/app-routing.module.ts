a90703456bb8d6356f2becf5368e2dcebc970c80
Tzappiello <tzappiello@gmail.com>
2022-06-28 18:43:15 -0300

    Rota criada para o component HOME

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
-    path: '', component: NavComponent
+    path: '', component: NavComponent, children: [
+      {path: 'home', component: HomeComponent}
+    ]
   }
 ];
 
