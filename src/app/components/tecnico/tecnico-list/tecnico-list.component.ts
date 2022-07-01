96ecd175bf6f24c14c9bda1d7b5cc746fa378706
lailaz <88406056+zallih@users.noreply.github.com>
2022-07-01 12:03:13 -0300

    Criação da tabela de tecnicos HTML, TS e CSS, e criação de Tecnico interface.

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Tecnico } from './../../../models/tecnico';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+  
+  ELEMENT_DATA: Tecnico [] = [
+    {
+      id: 1,
+      nome: 'Thiago Zappiello', 
+      cpf: '123.456.789-00',
+      email: 'tzappiello@email.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '12/07/2022'
+    }
+  ];
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
