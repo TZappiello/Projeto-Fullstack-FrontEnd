aac7a30730f175e068861d78f6d49c1b54505662
tzappiello <tzappiello@gmail.com>
2024-05-03 17:19:57 -0300

    Ajuste da porta para subir a aplicação

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     baseUrl: 'https://projeto-fullstack-backend-zvji.onrender.com'
-    //baseUrl: 'http://localhost:8080'
+    //baseUrl: 'http://localhost:8080'  esse é local
 }
